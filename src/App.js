3aa4ed1bd825010517e960542f072e728061a43f
Kemran1 <94720339+Kemran1@users.noreply.github.com>
2024-06-16 13:46:56 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
+import React, { useEffect } from 'react';
 import './App.css';
 
 const tg = window.Telegram.WebApp;
-function App() {
 
+function App() {
     useEffect(() => {
         tg.ready();
+
     }, []);
 
     const onClose = () => {
-        tg.close
+        tg.close();
 
     }
 
-  return (
-    <div className="App">
-      work
-        <button onClick={onClose}>Закрыть</button>
-    </div>
-  );
+    return (
+        <div className="App">
+            work
+            <button onClick={onClose}>Закрыть</button>
+        </div>
+    );
 }
 
 export default App;
